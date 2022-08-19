c160e4c43a657d22a011ca40f12390daf9f36d13
adam-maj <mr.adam.maj@gmail.com>
2022-08-19 14:15:18 -0400

    Update express setup with cookie parser

--- src/express/index.ts
+++ src/express/index.ts
@@ -3,11 +3,12 @@ import logoutHandler from "./routes/logout";
 import userHandler from "./routes/user";
 import { ThirdwebAuthConfig, ThirdwebAuthRoute } from "./types";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import cookieParser from "cookie-parser";
 import { Express, NextFunction, Request, Response } from "express";
 
 export * from "./types";
 
-export function withThirdwebAuth(app: Express, cfg: ThirdwebAuthConfig) {
+export function thirdwebAuth(app: Express, cfg: ThirdwebAuthConfig) {
   const ctx = {
     ...cfg,
     sdk: ThirdwebSDK.fromPrivateKey(cfg.privateKey, "mainnet"),
@@ -15,10 +16,12 @@ export function withThirdwebAuth(app: Express, cfg: ThirdwebAuthConfig) {
 
   const authUrl = cfg.authUrl?.replace(/\/$/, "") || "/auth";
 
+  app.use(cookieParser());
+
   app.use(async (req: Request, _: Response, next: NextFunction) => {
     const { sdk, domain } = ctx;
     let user = null;
-    const token = req.cookies?.thirdweb_auth_token;
+    const token = req.cookies.thirdweb_auth_token;
 
     if (token) {
       try {
@@ -36,13 +39,11 @@ export function withThirdwebAuth(app: Express, cfg: ThirdwebAuthConfig) {
   app.get(`${authUrl}/:route`, (req: Request, res: Response) => {
     const action = req.params.route as ThirdwebAuthRoute;
 
-    console.log("Caught: ", action);
-
     switch (action) {
       case "login":
         return loginHandler(req, res, ctx);
       case "user":
-        return userHandler(req, res, ctx);
+        return userHandler(req, res);
       case "logout":
         return logoutHandler(req, res);
       default:
--- src/express/routes/user.ts
+++ src/express/routes/user.ts
@@ -1,23 +1,5 @@
-import { ThirdwebAuthContext } from "../types";
 import { Request, Response } from "express";
 
-export default async function handler(
-  req: Request,
-  res: Response,
-  ctx: ThirdwebAuthContext
-) {
-  const { sdk, domain } = ctx;
-  let user = null;
-  const token = req.cookies.thirdweb_auth_token;
-
-  if (token) {
-    try {
-      const address = await sdk.auth.authenticate(domain, token);
-      user = { address };
-    } catch {
-      // No-op
-    }
-  }
-
-  return res.status(200).json(user);
+export default async function handler(req: Request, res: Response) {
+  return res.status(200).json(req.user);
 }
